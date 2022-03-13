a4d390a050cf07c1c7a30633b0591854bef4a71c
Hammad Rashid (Hammad) <76726810+Hammad1007@users.noreply.github.com>
2022-03-13 18:42:16 +0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,30 @@
+# Task 3
+def Func(x, y, c):
+  if c == '+':
+    try:
+      res = x + y;
+      #print(res);
+    except:
+      print("Masla");
+  elif c == '*':
+    try:
+      res = x * y;
+      #print(res);
+    except:
+      print("Masla");
+  elif c == '/':
+    try: 
+      res = x / y;
+      #print(res)
+    except: 
+      print("Masla");
 
+  return res;
+
+# Main driver
+a = 8;
+b = 2;
+print(Func(a, b, '/'));
+print(Func(a, b, '+'));
+print(Func(a, b, '*'));
+print(Func(a, 'abs', '+'));
