9ee1b1f49fdbe5c4b38d552f6dd80ff3f67f01d8
Hammad Rashid (Hammad) <76726810+Hammad1007@users.noreply.github.com>
2022-03-13 18:41:30 +0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,1 +1,18 @@
+# Task 2
+# Printing number of vowel in a set
 
+# funtion to find the vowels
+def Vowels(s):
+  count = 0;
+  vow = set("aeiouAEIOU")
+
+  for i in s:
+        if i in vow:
+            count = count + 1
+      
+  print("Number of vowels: ", count)
+
+string = 'Hammad Rashid'
+print("The string is: ", string);
+
+Vowels(string);
